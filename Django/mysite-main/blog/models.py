8f1f101cbcd8eab285dbc8c96f196c9c57c00537
Karen Moraes <karenmoraes222@gmail.com>
2025-09-08 10:38:54 -0300

    Alteração post

--- Django/mysite-main/blog/models.py
+++ Django/mysite-main/blog/models.py
@@ -3,14 +3,14 @@ from django.contrib.auth.models import User
 
 STATUS = (
     (0, 'Draft'),
-    (1, 'Publisher')
+    (1, 'Published')
 )
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
